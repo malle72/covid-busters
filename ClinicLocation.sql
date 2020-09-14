3209a806c505d639fb23167180a9d27103ec7db1
malle72 <42781131+malle72@users.noreply.github.com>
2020-09-14 11:34:11 -0500

    initial commit
    Commiting initial progress on all SQL files

---
+++ ClinicLocation.sql
@@ -0,0 +1,48 @@
+create table ClinicLocation (
+	clinicID INT,
+	name VARCHAR(50),
+	address VARCHAR(50),
+	city VARCHAR(50),
+	state VARCHAR(50),
+	zip VARCHAR(50)
+);
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (1, 'Smith Group Clinic', '1 Bonner Way', 'New Orleans', 'LA', '70154');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (2, 'Morar, Koch and Gerlach Clinic', '5803 Lake View Alley', 'Shreveport', 'LA', '71105');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (3, 'Gottlieb LLC Clinic', '052 Quincy Avenue', 'Lake Charles', 'LA', '70607');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (4, 'Fahey-Wintheiser Clinic', '6 Porter Street', 'Monroe', 'LA', '71208');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (5, 'Schoen, Daugherty and Becker Clinic', '029 Straubel Lane', 'New Orleans', 'LA', '70179');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (6, 'Daniel Group Clinic', '6 Browning Plaza', 'New Orleans', 'LA', '70187');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (7, 'Blanda, Hamill and Keeling Clinic', '66 4th Place', 'Monroe', 'LA', '71213');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (8, 'Konopelski Group Clinic', '505 Donald Crossing', 'New Orleans', 'LA', '70179');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (9, 'Stokes Inc Clinic', '2 Lien Drive', 'Baton Rouge', 'LA', '70815');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (10, 'Schmitt, Lockman and Herzog Clinic', '67619 Spohn Drive', 'Lafayette', 'LA', '70505');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (11, 'Keeling-Dooley Clinic', '32 Annamark Hill', 'Shreveport', 'LA', '71151');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (12, 'Boehm-Mosciski Clinic', '6346 Grasskamp Crossing', 'Shreveport', 'LA', '71105');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (13, 'Spinka-Greenfelder Clinic', '8665 Sutherland Road', 'Monroe', 'LA', '71213');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (14, 'Wyman-Stokes Clinic', '93063 North Pass', 'New Orleans', 'LA', '70160');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (15, 'Bailey Inc Clinic', '0238 Northview Park', 'Baton Rouge', 'LA', '70826');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (16, 'Kutch-McKenzie Clinic', '21066 Forest Dale Pass', 'Alexandria', 'LA', '71307');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (17, 'Hansen-Labadie Clinic', '07415 Ohio Plaza', 'Alexandria', 'LA', '71307');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (18, 'Schowalter Group Clinic', '74504 Melby Court', 'New Orleans', 'LA', '70179');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (19, 'Brekke-Koepp Clinic', '68 Commercial Road', 'Baton Rouge', 'LA', '70883');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (20, 'Eichmann Inc Clinic', '20 Gina Center', 'Metairie', 'LA', '70033');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (21, 'West, Ratke and Grady Clinic', '52027 David Plaza', 'Monroe', 'LA', '71213');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (22, 'Casper, Gibson and Little Clinic', '7493 East Park', 'New Orleans', 'LA', '70179');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (23, 'Hartmann-Kub Clinic', '500 Karstens Pass', 'New Orleans', 'LA', '70116');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (24, 'Runte-Bruen Clinic', '516 Talmadge Terrace', 'Monroe', 'LA', '71213');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (25, 'Ledner-Yundt Clinic', '0753 Kingsford Road', 'Metairie', 'LA', '70005');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (26, 'Kuvalis Inc Clinic', '03321 Graceland Point', 'Shreveport', 'LA', '71151');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (27, 'Powlowski and Sons Clinic', '4 Melvin Park', 'New Orleans', 'LA', '70183');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (28, 'Runolfsdottir Group Clinic', '3636 Thackeray Plaza', 'Baton Rouge', 'LA', '70826');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (29, 'Buckridge-Morar Clinic', '072 Orin Junction', 'Lafayette', 'LA', '70593');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (30, 'Murray-Bednar Clinic', '6 Ridgeview Circle', 'New Orleans', 'LA', '70179');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (31, 'Gulgowski, Gibson and Von Clinic', '7 Onsgard Center', 'Metairie', 'LA', '70005');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (32, 'VonRueden LLC Clinic', '32 Pierstorff Road', 'New Orleans', 'LA', '70179');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (33, 'Mann and Sons Clinic', '1018 Del Sol Junction', 'Lafayette', 'LA', '70505');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (34, 'Willms Inc Clinic', '2075 Doe Crossing Way', 'Monroe', 'LA', '71213');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (35, 'Hoeger, Wolff and Raynor Clinic', '179 Bobwhite Trail', 'Baton Rouge', 'LA', '70883');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (36, 'Boyer, Ruecker and Weissnat Clinic', '85 Tomscot Avenue', 'Metairie', 'LA', '70005');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (37, 'Mayer-Miller Clinic', '5389 Rutledge Place', 'Baton Rouge', 'LA', '70826');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (38, 'Koch Group Clinic', '01 Village Green Trail', 'New Orleans', 'LA', '70142');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (39, 'Kilback-Ortiz Clinic', '38470 Stephen Way', 'Alexandria', 'LA', '71307');
+insert into ClinicLocation (clinicID, name, address, city, state, zip) values (40, 'Jaskolski Group Clinic', '2176 Lien Alley', 'Alexandria', 'LA', '71307');
