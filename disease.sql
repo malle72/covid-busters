9871e4ab79df7f804efd1a49988126ce32567140
malle72 <42781131+malle72@users.noreply.github.com>
2020-09-14 13:14:20 -0500

    Tables created; Inserting data

--- disease.sql
+++ disease.sql
@@ -7,14 +7,14 @@ primary key (diseaseID)
 );
 
 
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (1, 'Desert Cold', 'Heloderma horridum', false);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (2, 'Numb Leprosy', 'Lepilemur rufescens', false);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (3, 'Deteriorating Arthritis', 'Papio ursinus', true);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (4, 'Stomach Anxiety', 'Cervus canadensis', true);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (5, 'Crystal Leprosy', 'Grus canadensis', false);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (6, 'Dream Panic', 'Ornithorhynchus anatinus', false);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (7, 'Avian Panic', 'Oxybelis fulgidus', true);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (8, 'Wolf Fever', 'Carduelis pinus', false);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (9, 'Autumn Malaria', 'Alopex lagopus', false);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (10, 'Iron Panic', 'Bradypus tridactylus', false);
-insert into disease (diseaseID, commonName, scientificName, requiresReferral, notes) values (10, 'Covid-19', 'Sars-Cov-2', true);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (1, 'Desert Cold', 'Heloderma horridum', false);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (2, 'Numb Leprosy', 'Lepilemur rufescens', false);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (3, 'Deteriorating Arthritis', 'Papio ursinus', true);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (4, 'Stomach Anxiety', 'Cervus canadensis', true);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (5, 'Crystal Leprosy', 'Grus canadensis', false);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (6, 'Dream Panic', 'Ornithorhynchus anatinus', false);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (7, 'Avian Panic', 'Oxybelis fulgidus', true);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (8, 'Wolf Fever', 'Carduelis pinus', false);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (9, 'Autumn Malaria', 'Alopex lagopus', false);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (10, 'Iron Panic', 'Bradypus tridactylus', false);
+insert into disease (diseaseID, commonName, scientificName, requiresReferral) values (11, 'Covid-19', 'Sars-Cov-2', true);
